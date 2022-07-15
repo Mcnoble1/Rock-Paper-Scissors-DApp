e0bf846e6121d960b34acd1315b6602228685e60
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-07-15 23:19:26 +0100

    updated frontend with randomness :octocat:

--- index.mjs
+++ index.mjs
@@ -6,7 +6,10 @@ const startingBalance = stdlib.parseCurrency(100);
 const accAlice = await stdlib.newTestAccount(startingBalance);
 const accMike = await stdlib.newTestAccount(startingBalance);
 
-const fmt = (x) => stdlib.formatCurrency(startingBalance);
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+const beforeAlice = await getBalance(accAlice);
+const beforeMike = await getBalance(accMike);
 
 const ctcAlice = accAlice.contract(backend); // Alice is the deployer of the contract by calling the backend
 const ctcMike = accMike.contract(backend, ctcAlice.getInfo());
@@ -14,6 +17,7 @@ const ctcMike = accMike.contract(backend, ctcAlice.getInfo());
 const HAND = ['Rock', 'Paper', 'Scissors'];
 const OUTCOME = ['Mike Wins', 'Draw', 'Alice wins'];
 const Player = (Who) => ({
+    ...stdlib.hasRandom,
     getHand: () => {
         const hand = Math.floor(Math.random() * 3);
         console.log(`${Who} played ${HAND[hand]}`);
@@ -29,9 +33,21 @@ await Promise.all([
     ctcAlice.p.Alice({
         // interact object here
         ...Player('Alice'),
+        wager: stdlib.parseCurrency(5),
     }),
     ctcMike.p.Mike({
         // interact object here
         ...Player('Mike'),
-    }),
+        acceptWager: (amt) => {
+            console.log(`Mike accepted wager of ${fmt(amt)}`);
+            return null;
+        }
+        }
+    ),
 ]);
+
+const afterAlice = await getBalance(accAlice);
+const afterMike = await getBalance(accMike);
+
+console.log(`Alice went from ${beforeAlice} to ${afterAlice}`);
+console.log(`Mike went from ${beforeMike} to ${afterMike}`);
